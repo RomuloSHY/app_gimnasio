ad969e725116dfcc3da1127dc7b2924a58c35b83
RomuloSHY <stin.huaman.yalo@gmail.com>
2026-02-11 10:32:19 -0500

    Agrega configuracion CRUD de clase Membresia y JSONs de prueba

--- Backend-Springboot/app_gimnasio/src/main/java/com/proyecto/app_gimnasio/controller/MembresiaController.java
+++ Backend-Springboot/app_gimnasio/src/main/java/com/proyecto/app_gimnasio/controller/MembresiaController.java
@@ -1,4 +1,43 @@
 package com.proyecto.app_gimnasio.controller;
 
+import com.proyecto.app_gimnasio.entity.Membresia;
+import com.proyecto.app_gimnasio.service.MembresiaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/membresia")
 public class MembresiaController {
+
+    private final MembresiaService membresiaService;
+
+    public MembresiaController(MembresiaService membresiaService){
+        this.membresiaService = membresiaService;
+    }
+
+    @GetMapping
+    public List<Membresia> listarMembresias(){
+        return membresiaService.listarMembresias();
+    }
+
+    @GetMapping("/{id}")
+    public Membresia buscarMembresiaPorId(@PathVariable Long id){
+        return membresiaService.buscarMembresiaPorId(id);
+    }
+
+    @PostMapping
+    public Membresia agregarMembresia(@RequestBody Membresia membresia){
+        return membresiaService.agregarMembresia(membresia);
+    }
+
+    @PutMapping("/{id}")
+    public Membresia modificarMembresia(@PathVariable Long id, @RequestBody Membresia membresia){
+        return membresiaService.modificarMembresia(id, membresia);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarMembresia(@PathVariable Long id){
+        membresiaService.eliminarMembresiaPorId(id);
+    }
 }
--- Backend-Springboot/app_gimnasio/src/main/java/com/proyecto/app_gimnasio/service/MembresiaService.java
+++ Backend-Springboot/app_gimnasio/src/main/java/com/proyecto/app_gimnasio/service/MembresiaService.java
@@ -1,7 +1,10 @@
 package com.proyecto.app_gimnasio.service;
 
+import com.proyecto.app_gimnasio.entity.Membresia;
 import com.proyecto.app_gimnasio.repository.MembresiaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MembresiaService {
@@ -10,5 +13,36 @@ public class MembresiaService {
 
     public MembresiaService(MembresiaRepository membresiaRepository){
         this.membresiaRepository = membresiaRepository;
+    }
+
+    public List<Membresia> listarMembresias(){
+        return membresiaRepository.findAll();
+    }
+
+    public Membresia buscarMembresiaPorId(Long id){
+        return membresiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Membresia no encontrada"));
+    }
+
+    public Membresia agregarMembresia(Membresia membresiaNueva){
+        return membresiaRepository.save(membresiaNueva);
+    }
+
+    public Membresia modificarMembresia(Long id, Membresia membresiaActual){
+        Membresia membresiaModificada = membresiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Membresia no encontrada"));
+
+        membresiaModificada.setTipo(membresiaActual.getTipo());
+        membresiaModificada.setPrecio(membresiaActual.getPrecio());
+        membresiaModificada.setDuracionMeses(membresiaActual.getDuracionMeses());
+
+        return membresiaRepository.save(membresiaModificada);
+    }
+
+    public void eliminarMembresiaPorId(Long id){
+        Membresia membresiaEliminada = membresiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Membresia no encontrada"));
+
+        membresiaRepository.delete(membresiaEliminada);
     }
 }
